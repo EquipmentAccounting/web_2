d252e92e4c895760f6d2c1114431ffeb7e238b6c
Goddo-ro <r0manovic1@yandex.ru>
2024-03-07 13:52:07 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { externalizeDeps } from 'vite-plugin-externalize-deps';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/web/',
   plugins: [react()],
   resolve: {
     alias: {
